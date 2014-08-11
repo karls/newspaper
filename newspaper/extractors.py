400ff02c6bc84a8fa5a4a866abfaa02168415f06
Karl Sutt <karl@sutt.ee>
2014-08-11 15:00:29 +0100

    Another

--- newspaper/extractors.py
+++ newspaper/extractors.py
@@ -21,7 +21,7 @@ import urlparse
 
 from . import urls
 
-from dateutil import parse
+from dateutil.parser import parse
 
 from .packages.tldextract import tldextract
 from .utils import ReplaceSequence, StringReplacement, StringSplitter
